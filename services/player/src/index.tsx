1c7da86370d3dddf715474f0c189361f17bf075c
Дмитрий Соловьев <DmSolovev@mbs.media>
2024-01-23 16:12:21 +0200

    migrate to webpack

--- services/player/src/index.tsx
+++ services/player/src/index.tsx
@@ -1,16 +1,15 @@
-import { version } from "../package.json";
+import version from "../package.json";
 
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.tsx";
+import { App } from "./App";
 
-import "./index.css";
-import { store } from "./store/index.ts";
-import { sendEvent } from "./store/action.ts";
+import { store } from "./store";
+import { sendEvent } from "./store/action";
 import { Provider } from "react-redux";
-import { canUseAviF } from "./utils/canUseAvif.ts";
+import { canUseAviF } from "./utils/canUseAvif";
 
-window.appVersion = version;
+window.appVersion = version.version;
 
 store.dispatch(sendEvent({ type: "DO_INIT" }));
 
